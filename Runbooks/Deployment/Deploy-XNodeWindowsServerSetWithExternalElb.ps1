71a500eb102474bff12530653967672649790f1d
CharlPels <charl@cpels.nl>
2017-02-09 21:03:06 +0100

    Updated Deploy-XNodeWindowsServerSetWithExternalElb.ps1 in Azure Automation.

--- Runbooks/Deployment/Deploy-XNodeWindowsServerSetWithExternalElb.ps1
+++ Runbooks/Deployment/Deploy-XNodeWindowsServerSetWithExternalElb.ps1
@@ -13,10 +13,6 @@ param(
     [Parameter(Mandatory=$true)]
     [String] $vmNamePrefix,
 
-    #Storage account name must be between 3 and 24 characters in length and use numbers and lower-case letters only.
-    [Parameter(Mandatory=$true)]
-    [String] $StorageAccountName,
-
     [Parameter(Mandatory=$true)]
     [string] $NetworkparametersnetworkSubnetName,
 
@@ -220,7 +216,6 @@ $Azureparameters.Add("VmSize", $VmSize)
 $Azureparameters.Add("vmNamePrefix", $vmNamePrefix)
 $Azureparameters.Add("AdminUserName", $AdminUserName)
 $Azureparameters.Add("AdminPassword", $AdminPassword)
-$Azureparameters.Add("ServersStorageName", $StorageAccountName) #Storage account name must be between 3 and 24 characters in length and use numbers and lower-case letters only.
 $Azureparameters.Add("numberOfInstances", $numberOfInstances)
 $Azureparameters.Add("WindowsOSVersion", $WindowsOSVersion)
 $Azureparameters.Add("virtualNetworkResourceGroup", (Get-AutomationVariable -Name NetworkparametersResourceGroupName))
@@ -238,7 +233,7 @@ $Azureparameters.Add("virtualNetworkSubnetName", $NetworkparametersnetworkSubnet
 $Azureparameters.Add("loadBalancerName", $loadBalancerName)
 $Azureparameters.Add("dnsNameforLBIP", $dnsNameforLBIP)
 
-#Create the AD Connect for o365 resource group
+#Create the resource group
 New-AzureRmResourceGroup -Name $AzureResourceGroupName -Location $azurelocation -Verbose -Force -ErrorAction Stop
 
 #generate template url
