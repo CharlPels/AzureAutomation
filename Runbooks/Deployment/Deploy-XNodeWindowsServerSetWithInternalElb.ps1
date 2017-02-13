587010de8994059489c19852afc9cd2ed6f55f4d
CharlPels <charl@cpels.nl>
2017-02-13 13:30:36 +0100

    Updated Deploy-XNodeWindowsServerSetWithInternalElb.ps1 in Azure Automation.

--- Runbooks/Deployment/Deploy-XNodeWindowsServerSetWithInternalElb.ps1
+++ Runbooks/Deployment/Deploy-XNodeWindowsServerSetWithInternalElb.ps1
@@ -236,8 +236,14 @@ $Azureparameters.Add("loadBalancerIP", $loadBalancerIP)
 #Create the AD Connect for o365 resource group
 New-AzureRmResourceGroup -Name $AzureResourceGroupName -Location $azurelocation -Verbose -Force -ErrorAction Stop
 
+#Managed storage is not yet supporing encryption and backup at the same time so we need managed storage withoud altering the templates
+$TemplateUri  = $baseuri + "VirtualServers/" + "GetFreeStorageName.json" + $SAScontainertoken
+$storagename = new-AzureRmResourceGroupDeployment -ResourceGroupName $AzureResourceGroupName  -TemplateUri $TemplateUri
+$storagename.Outputs.Values.Value
+$Azureparameters.Add("ServersstorageName", $storagename.Outputs.Values.Value)
+
 #generate template url
-$TemplateUri  = $baseuri + "VirtualServers/" + "GenericxNodePairWithInternalLoadBalancer.json" + $SAScontainertoken
+$TemplateUri  = $baseuri + "VirtualServers/" + "GenericxNodePairWithInternalLoadBalancer-storageaccount.json" + $SAScontainertoken
 
 Test-AzureRmResourceGroupDeployment -ResourceGroupName $AzureResourceGroupName  -TemplateUri $TemplateUri -TemplateParameterObject $Azureparameters
 New-AzureRmResourceGroupDeployment -Name "automation"  -ResourceGroupName $AzureResourceGroupName -TemplateUri $TemplateUri -TemplateParameterObject $Azureparameters -Verbose
